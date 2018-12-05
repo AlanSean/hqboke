c8ceff8bca38cf7e2808c72176cab120a6365ee6
Mr.huang <2316694914@qq.com>
2018-12-05 10:52:11 +0800

    serviceWorker缓存测试

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('/cache-polyfill.js');
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'www.hqboke.com-v';
 var urlsToCache = [
     '/',
     '/index.html',
@@ -10,35 +10,30 @@ var urlsToCache = [
     '/favicon.jpg',
     '/manifest.json',
 ];
-
-self.addEventListener('install', function(event) {
-  // 执行安装步骤
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-self.addEventListener('activate', function(event) {
-    // event.waitUntil(
-    //     Promise.all([
-    //         // 清理旧版本
-    //         caches.keys().then(function (cacheList) {
-    //             return Promise.all(
-    //                 cacheList.map(function (cacheName) {
-    //                     console.log('清理',cacheName);
-    //                     return caches.delete(cacheName);
-    //                 })
-    //             );
-    //         })
-    //     ])
-    // );
-});
-self.addEventListener('fetch', function(event) {
-    // event.request['accept-encoding']= 'gzip, deflate, br';
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+caches.keys().then(function (cacheList) {
+    var v = cacheList.length>0 ?  Number(cacheList[0].split(CACHE_NAME)[1]) : 1;
+    return {
+        name:CACHE_NAME + (v+1),
+        oldname:CACHE_NAME+v
+    };
+}).then( res:{catch_name,old_catch_name} => {
+    self.addEventListener('install', function(event) {
+      // 执行安装步骤
+        event.waitUntil(
+            caches.open(catch_name).then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+        );
+    });
+    self.addEventListener('activate', function(event) {
+        console.log('清理old_catch_name',old_catch_name);
+        caches.delete(old_catch_name);
+    });
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    });
+})
