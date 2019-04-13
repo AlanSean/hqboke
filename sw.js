25b6912d51157942d6af41a9b27a7e2338de89f0
Mr.huang <2316694914@qq.com>
2019-04-13 13:27:43 +0800

    删除缓存 html

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 "use strict";
 var precacheConfig = [
-        ['/','0.02'],
-        ['index.html','0.05'],
         ['css/index.css','0.05'],
         ['js/jq.js','0.05'],
         ['js/powder.js','0.03'],
