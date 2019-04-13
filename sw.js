a8db2e15470012f87ee12ab1bd9edb5d5bb4148f
Mr.huang <2316694914@qq.com>
2019-04-13 13:25:21 +0800

    更新缓存

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 "use strict";
 var precacheConfig = [
         ['/','0.02'],
-        ['index.html','0.03'],
-        ['css/index.css','0.04'],
+        ['index.html','0.04'],
+        ['css/index.css','0.05'],
         ['js/jq.js','0.05'],
         ['js/powder.js','0.03'],
         ['js/index.js','0.07'],
